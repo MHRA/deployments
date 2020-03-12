f05590a6e0ac7d24fa9d9c3a7f7f95f6483c837a
Stuart Harris <stuartharris@Stuarts-MacBook-Pro.local>
2020-03-12 14:38:52 +0000

    istio manifests via istioctl manifest generate

---
+++ istio/certs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -subj '/O=argocd-mhra/CN=red-badger.dev' -keyout root-key.txt -out root-crt.txt
+
+openssl req -out csr.txt -newkey rsa:2048 -nodes -keyout key.txt -subj "/CN=argocd-mhra.red-badger.dev/O=argocd-mhra"
+openssl x509 -req -days 365 -CA root-crt.txt -CAkey root-key.txt -set_serial 0 -in csr.txt -out crt.txt
+
+kubectl create -n istio-system secret tls istio-ingressgateway-certs --key key.txt --cert crt.txt
