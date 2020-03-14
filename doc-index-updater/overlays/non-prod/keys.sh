0915716f542973f1e27b80cc8d350f9a113ef848
Stuart Harris <stuartharris@Stuarts-MBP.lan>
2020-03-14 12:58:20 +0000

    update env vars for doc-index-updater

--- doc-index-updater/overlays/non-prod/keys.sh
+++ doc-index-updater/overlays/non-prod/keys.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
-kubectl create secret generic sentinel-creds \
+# Redis credentials...
+REDIS_KEY=$(az redis list-keys \
+    --resource-group MHRA-dev \
+    --name doc-index-updater-dev \
+    --output tsv --query 'primaryKey')
+kubectl create secret generic redis-creds \
     -n doc-index-updater \
     -o json \
     --dry-run \
-    --from-literal server="127.0.0.1" \
-    --from-literal user="insert here" \
-    --from-literal pass="insert here" |
+    --from-literal key="$REDIS_KEY" |
     kubeseal \
-        --format yaml >SealedSecret-sentinel-creds.yaml
+        --format yaml >SealedSecret-redis-creds.yaml
 
-kubectl create secret generic storage-creds \
-    -n doc-index-updater \
-    -o json \
-    --dry-run \
-    --from-literal account="insert here" \
-    --from-literal container="insert here" \
-    --from-literal key="insert here" |
-    kubeseal \
-        --format yaml >SealedSecret-storage-creds.yaml
-
+# Azure Search Service credentials...
 kubectl create secret generic search-creds \
     -n doc-index-updater \
     -o json \
@@ -30,3 +24,51 @@ kubectl create secret generic search-creds \
     --from-literal query_key="insert here" |
     kubeseal \
         --format yaml >SealedSecret-search-creds.yaml
+
+# Sentinel credentials...
+kubectl create secret generic sentinel-creds \
+    -n doc-index-updater \
+    -o json \
+    --dry-run \
+    --from-literal server="127.0.0.1" \
+    --from-literal user="insert here" \
+    --from-literal pass="insert here" |
+    kubeseal \
+        --format yaml >SealedSecret-sentinel-creds.yaml
+
+# Azure Service Bus credentials...
+SB_CREATE_KEY=$(az servicebus queue authorization-rule keys list \
+    --resource-group MHRA-dev \
+    --namespace-name doc-index-updater-dev \
+    --queue-name doc-index-updater-create-queue \
+    --name doc-index-updater-create-auth \
+    --query primaryKey \
+    --output tsv)
+SB_DELETE_KEY=$(
+    az servicebus queue authorization-rule keys list \
+        --resource-group MHRA-dev \
+        --namespace-name doc-index-updater-dev \
+        --queue-name doc-index-updater-delete-queue \
+        --name doc-index-updater-delete-auth \
+        --query primaryKey \
+        --output tsv
+)
+kubectl create secret generic service-bus-creds \
+    -n doc-index-updater \
+    -o json \
+    --dry-run \
+    --from-literal create_key="$SB_CREATE_KEY" \
+    --from-literal delete_key="$SB_DELETE_KEY" |
+    kubeseal \
+        --format yaml >SealedSecret-service-bus-creds.yaml
+
+# Azure Blob Storage credentials...
+kubectl create secret generic storage-creds \
+    -n doc-index-updater \
+    -o json \
+    --dry-run \
+    --from-literal account="insert here" \
+    --from-literal container="insert here" \
+    --from-literal key="insert here" |
+    kubeseal \
+        --format yaml >SealedSecret-storage-creds.yaml
