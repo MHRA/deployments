5e04e87230ba4cb87d34eec59a4923e538757f92
Tim Lee <tim.lee@red-badger.com>
2020-08-26 17:41:55 +0100

    Allow ingress from test wildcard cert too

--- doc-index-updater/overlays/non-prod/keys.sh
+++ doc-index-updater/overlays/non-prod/keys.sh
@@ -138,3 +138,25 @@ kubectl create secret generic basic-auth-creds \
     --from-literal password="$BASIC_AUTH_PASSWORD" |
     kubeseal \
         --format yaml >SealedSecret-basic-auth-creds.yaml
+
+# Pull and generate wildcard certificate for *.test
+az keyvault secret show \
+    --vault-name mhra-non-prod-02 \
+    --name doc-index-updater-mhra-gov-uk-cer \
+    --query value \
+    --output tsv >doc-index-updater.crt
+az keyvault secret show \
+    --vault-name mhra-non-prod-02 \
+    --name doc-index-updater-mhra-gov-uk-key \
+    --query value \
+    --output tsv >doc-index-updater.key
+kubectl create secret tls test-wildcard-cert \
+    -n istio-system \
+    -o json \
+    --dry-run \
+    --cert=./doc-index-updater.crt \
+    --key=./doc-index-updater.key |
+    kubeseal \
+        --format yaml >Sealed-secret-test-wildcard-cert.yaml
+rm doc-index-updater.crt
+rm doc-index-updater.key
