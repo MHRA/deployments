b61564759841ae1d04b6007f2d08bd195169e9dd
Tim Lee <tim.lee@red-badger.com>
2020-08-27 09:46:50 +0100

    Add doc index updater new prod config for gateways and 80 route for medicines api prod

--- doc-index-updater/overlays/prod-new/keys.sh
+++ doc-index-updater/overlays/prod-new/keys.sh
@@ -138,3 +138,25 @@ kubectl create secret generic basic-auth-creds \
     --from-literal password="$BASIC_AUTH_PASSWORD" |
     kubeseal \
         --format yaml >SealedSecret-basic-auth-creds.yaml
+
+# Pull and generate certificate for doc-index-updater.mhra.gov.uk
+az keyvault secret show \
+    --vault-name mhraproducts4853 \
+    --name doc-index-updater-mhra-gov-uk-cer \
+    --query value \
+    --output tsv >doc-index-updater.crt
+az keyvault secret show \
+    --vault-name mhraproducts4853 \
+    --name doc-index-updater-mhra-gov-uk-key \
+    --query value \
+    --output tsv >doc-index-updater.key
+kubectl create secret tls issued-doc-index-updater-cert \
+    -n istio-system \
+    -o json \
+    --dry-run \
+    --cert=./doc-index-updater.crt \
+    --key=./doc-index-updater.key |
+    kubeseal \
+        --format yaml >SealedSecret-issued-doc-index-updater-cert.yaml
+rm doc-index-updater.crt
+rm doc-index-updater.key
